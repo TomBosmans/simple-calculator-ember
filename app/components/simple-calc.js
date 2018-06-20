d20f30b878d940df3d75e2bffba3a971222be5bf
TomBosmans <t.bosse@hotmail.com>
2018-06-20 17:56:35 +0200

    removed the use of jquery

--- app/components/simple-calc.js
+++ app/components/simple-calc.js
@@ -1,26 +1,25 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import $ from 'jquery'
 
 export default Component.extend({
-  calculator: service('calculator'),
+  numbers: [],
+  result: 0,
 
   actions: {
     addNumber() {
-      var $input = $('.js-input');
-      var $list = $('.js-list');
-      var $result = $('.js-result');
-      var number = parseInt($input.val());
-      var calculator = this.get('calculator');
-
-      if (Number.isInteger(number)) {
-        calculator.add(number);
-        $input.val('');
-        $result.html(calculator.get('result'));
-        $list.append($("<li></li>").text(number));
+      var newNumber = parseInt(this.get('number'));
+      if (Number.isInteger(newNumber)) {
+	this.get('numbers').pushObject(newNumber);
+	this.set('number', null);
+	this.calculate();
       } else {
         alert('not a number');
       }
-    }
+    },
+  },
+
+  calculate() {
+    const sumReducer = (a, b) => a + b;
+    var sum = this.get('numbers').reduce(sumReducer, 0);
+    this.set('result', sum);
   }
 });
